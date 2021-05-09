2974695905fe40a8b4c50fb8a920482453dd7103
tomoya0417 <batayan0281@gmail.com>
2021-05-09 14:57:09 +0900

    proaca_react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
+import React, { useState } from 'react';
+import './App.css';
+
 function App() {
-const clickMe=()=>{
-  window.alert("Hello world")
-}
+  const clickMe = () => {
+    window.alert("Hello world")
+
+    const name = 'tabata';
+  }
+
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    console.log(count);
+  }
+
   return (
-       
-        <div>
-          <button onClick={clickMe}>
-            Hello Word
+    <div>
+      <button onClick={increment}>いいね！</button>
+      {count}
+      <div>
+        <button onClick={clickMe}>
+          Hello Word
           </button>
-        </div>
-    
+
+        <h1>Hello world</h1>
+        <h2>Hello world</h2>
+        <h3>Hello world</h3>
+        <h4>Hello world</h4>
+
+        <button
+          onClick={() => window.alert("Hello world")}
+        >button</button>
+
+        <button>button</button>
+        <input type="text" />
+        <a href="#">a タグ</a>
+
+
+
+      </div>
+    </div>
 
   )
 }
