ed42d1a5271a69294370474cc6111fd1ba77a914
田畑友也 <batay@MacBook-Air.local>
2021-06-04 23:35:13 +0900

    react-tomoya

--- src/components/ComponentB.js
+++ src/components/ComponentB.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
+import { Table } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 const ComponentB = () =>{
+  const [data, setData] = useState()
+  useEffect(() => {
+    console.log('useEffect が呼び出されました。 ');
+  
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(res => {
+      console.log(res)
+      setData(res.data)
+    })
+  }, []);
+  
   return(
     <div>
       <div>ComponentB</div>
       <Link to="componentc">ComponentCへ移動</Link>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>userId</th>
+            <th>title</th>
+            <th>body</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data && data.map((d, index) => {
+              return (
+                <tr>
+                  <td>{d.id}</td>
+                  <td>{d.userId}</td>
+                  <td>{d.title}</td>
+                  <td>{d.body.toString()}</td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </Table>
     </div>
   );
 };
--- src/components/ComponentC.js
+++ src/components/ComponentC.js
@@ -1,15 +1,62 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { Table, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
 
 const ComponentC = () =>{
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    console.log('useEffect が呼び出されました。');
+})
+
+const getData = () => {
+  axios.get('http://jsonplaceholder.typicode.com/comments')
+  .then(res => {
+    console.log(res)
+    setData(res.data)
+  })
+}
+
   return(
     <div>
       <div>ComponentC</div>
+      <Button variant="success" onClick={getData}>getData</Button>{' '}
       <Link to="/">ComponentAへ戻る</Link>
+    
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>postId</th>
+          <th>id</th>
+          <th>name</th>
+          <th>email</th>
+          <th>body</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {
+          data && data.map((d, index) => {
+
+            return (
+             <tr key={index}>
+               <td>{d.postId}</td>
+               <td>{d.id}</td>
+               <td>{d.name}</td>
+               <td>{d.email}</td>
+               <td>{d.body.toString()}</td>
+             </tr>
+            );
+          })
+        }
+      </tbody>
+    </Table>
+
     </div>
   );
 };
-
-
 
 export default ComponentC;
