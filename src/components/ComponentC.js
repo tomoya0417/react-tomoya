b95d46efc72005f01804bdaf4114480c9d98fd79
田畑友也 <batay@MacBook-Air.local>
2021-06-05 16:14:20 +0900

    react-tomoya

--- src/components/ComponentC.js
+++ src/components/ComponentC.js
@@ -1,30 +1,62 @@
-import React, {useState, useEffect } from 'react';
+import React, {useState, useEffect,useReducer } from 'react';
 import { Link } from 'react-router-dom';
 import { Table, Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import reducer from '../reducers/index';
 
+const initialCount = {
+  count: 0
+};
 
 const ComponentC = () =>{
   const [data, setData] = useState()
+  const [ state, dispatch ] = useReducer(reducer,initialCount);
 
   useEffect(() => {
     console.log('useEffect が呼び出されました。');
-})
+  })
 
 const getData = () => {
   axios.get('http://jsonplaceholder.typicode.com/comments')
   .then(res => {
     console.log(res)
     setData(res.data)
-  })
-}
+})};
+
+
+  const increment = () =>{
+    console.log('increment');
+    dispatch({
+      type: 'INCREMENT'
+    });
+  };
+
+    const decrement = () =>{
+      console.log('decrenent');
+      dispatch({
+        type: 'DECREMENT'
+      });
+  };
+
+  const recrement = () =>{
+    console.log('reset');
+    dispatch({
+      type: 'RESET'
+    });
+};
+
 
   return(
     <div>
       <div>ComponentC</div>
       <Button variant="success" onClick={getData}>getData</Button>{' '}
       <Link to="/">ComponentAへ戻る</Link>
+
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={recrement}>reset</button>
+      <h1>{state.count}</h1>
     
     <Table striped bordered hover>
       <thead>
